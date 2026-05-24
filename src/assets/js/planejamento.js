ee8e8da8db6450c22e409d5ca8f99e09cc055c2d
Vivian Nascimento Silva Amorim <viviannsamorim@gmail.com>
2026-05-24 20:57:41 -0300

    Add files via upload

---
+++ src/assets/js/planejamento.js
@@ -0,0 +1,14 @@
+function abrirModal(){
+
+    document
+        .getElementById("modal")
+        .style.display = "flex";
+}
+
+function fecharModal(){
+
+    document
+        .getElementById("modal")
+        .style.display = "none";
+}
+
